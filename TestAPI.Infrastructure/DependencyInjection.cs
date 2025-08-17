b7049cf176ee73c9a8ca9f3dfaa0cedd6936cabf
jayandradeph <act.jandrade@gmail.com>
2025-08-17 17:16:40 +0800

    Added DB and Dependency Injections

---
+++ TestAPI.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestAPI.Infrastructure.Persistence;
+
+namespace TestAPI.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
+        {
+            // DbContext
+            services.AddDbContext<TestAPIContext>(options =>
+                options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+
+            // Repositories
+            //services.AddScoped<IUserRepository, UserRepository>();
+            //services.AddScoped<IOrderRepository, OrderRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            // Services
+            //services.AddScoped<IUserService, UserService>();
+            //services.AddScoped<IOrderService, OrderService>();
+
+            return services;
+        }
+    }
+}
--- TestAPI/Program.cs
+++ TestAPI/Program.cs
@@ -1,6 +1,10 @@
+using TestAPI.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
